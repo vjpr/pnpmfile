f772e02f3a9828709856494cba8149fe0bf5698c
Vaughan Rouesnel <vrouesnel@gmail.com>
2018-03-21 19:13:31 +0100

    Update

--- packages/preset-create-react-native-app/def.js
+++ packages/preset-create-react-native-app/def.js
@@ -1,7 +1,7 @@
 export default {
 
   'react-native-scripts': {
-    dependencies: {
+    peerDependencies: {
       expo: '*',
     },
   },
--- packages/preset-create-react-native-app/index.js
+++ packages/preset-create-react-native-app/index.js
@@ -3,8 +3,14 @@ import reactNative from '@pnpmfile/react-native'
 import expo from '@pnpmfile/expo'
 import def from './def'
 
-// NOTE: To see crna packager errors you must add `console.log(JSON.stringify(msg, null, 2))` to `this._handlePackagerEvent` here:
-//   photo-booth-ios-crna/node_modules/.registry.npmjs.org/xdl/48.0.2/node_modules/xdl/build/logs/PackagerLogsStream.js
+// NOTE: To see crna packager errors you must add
+//   `console.log(JSON.stringify(msg, null, 2))`
+//   to `this._handlePackagerEvent` here:
+//   node_modules/.registry.npmjs.org/xdl/48.0.2/node_modules/xdl/build/logs/PackagerLogsStream.js
+
+// NOTE: To see full errors from the packager:
+//   Modify `handleError` in node_modules/.registry.npmjs.org/react-native/0.52.0/node_modules/react-native/local-cli/cliEntry.js:41
+//   See https://github.com/facebook/react-native/issues/18436
 
 export default function(pkg) {
   update(def)(pkg)
